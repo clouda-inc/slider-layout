3b7904223cc6a4b70902f2d0635e8379fb5acf9b
victorhmp <victor2142@gmail.com>
2020-08-01 15:47:22 -0300

    Move useSliderVisibility to a separate file

--- react/components/SliderTrack.tsx
+++ react/components/SliderTrack.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useEffect, useRef } from 'react'
+import React, { FC } from 'react'
 import { useCssHandles, applyModifiers } from 'vtex.css-handles'
 
 import { useSliderState, useSliderDispatch } from './SliderContext'
+import { useSliderVisibility } from '../hooks/useSliderVisibility'
 
 const CSS_HANDLES = ['sliderTrack', 'slide', 'slideChildrenContainer'] as const
 
@@ -9,25 +10,6 @@ interface Props {
   totalItems: number
   infinite: boolean
   usePagination: boolean
-}
-
-const isSlideVisible = ({
-  index,
-  currentSlide,
-  slidesToShow,
-  totalItems,
-}: {
-  index: number
-  currentSlide: number
-  slidesToShow: number
-  totalItems: number
-}): boolean => {
-  const isClonedSlide = currentSlide < 0 || currentSlide >= totalItems
-
-  return (
-    (index >= currentSlide && index < currentSlide + slidesToShow) ||
-    isClonedSlide
-  )
 }
 
 const resolveAriaAttributes = (
@@ -62,38 +44,6 @@ const getFirstOrLastVisible = (slidesPerPage: number, index: number) => {
   }
 
   return ''
-}
-
-const useSliderVisibility = (
-  currentSlide: number,
-  slidesPerPage: number,
-  totalItems: number
-) => {
-  /** Keeps track of slides that have been visualised before.
-   * We want to keep rendering them because the issue is mostly rendering
-   * slides that might never be viewed; On the other hand, hiding slides
-   * that were visible causes visual glitches */
-  const visitedSlides = useRef<Set<number>>(new Set())
-
-  useEffect(() => {
-    for (let i = 0; i < slidesPerPage; i++) {
-      visitedSlides.current.add(currentSlide + i)
-    }
-  }, [currentSlide, slidesPerPage])
-
-  const isItemVisible = (index: number) =>
-    isSlideVisible({
-      index,
-      currentSlide,
-      slidesToShow: slidesPerPage,
-      totalItems,
-    })
-
-  const shouldRenderItem = (index: number) => {
-    return visitedSlides.current.has(index) || isItemVisible(index)
-  }
-
-  return { shouldRenderItem, isItemVisible }
 }
 
 const SliderTrack: FC<Props> = ({ totalItems, infinite, usePagination }) => {
